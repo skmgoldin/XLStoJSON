e8c47fce83ed7b026a77ef01a2d2297271fed577
Mike Goldin <michaelgoldin@michaels-mbp.home>
2014-08-22 17:07:30 -0400

    Add output file as an argument; made seperate static method for printing to output.

--- ExcelReaderTester.java
+++ ExcelReaderTester.java
@@ -1,21 +1,30 @@
 import java.io.*;
 import org.json.*;
-import java.util.Set;
 
 public class ExcelReaderTester {
   
   public static void main(String[] args) {
     File inFile = new File(args[0]);
-    File outputFile = new File("output.txt");
+    File outputFile = new File(args[1]);
+    
+    // Parses the Excel into JSON.
     JSONObject parsedData = ExcelReader.parseExcel(inFile);
     
+    jPrinter(parsedData, outputFile);
+  }
+  
+  /**
+   * Prints a JSONObject to a file.
+   * @param json The JSONObject to be printed.
+   * @param output The output file.
+   */
+  private static void jPrinter(JSONObject json, File output) {
     try {
-    FileWriter writer = new FileWriter(outputFile);
-    writer.write(parsedData.toString(2));
-    writer.close();
-    } catch (IOException e) {
-        System.err.println("IO Exception");
-    }
-        
+      FileWriter writer = new FileWriter(output);
+      writer.write(json.toString(2));
+      writer.close();
+      } catch (IOException e) {
+          System.err.println("IO Exception");
+      }    
   }
 }
